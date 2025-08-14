82860b8e0d357682005afd590804e20821b7c10e
zhangxb <863232387@qq.com>
2025-08-14 17:36:27 +0800

    feat(xxl-job-plus): 添加 admin 模块并创建启动应用- 新增 xxl-job-plus-admin 模块的 pom.xml 文件- 在根 pom.xml 中添加 admin 模块引用 - 创建 XxlJobAdminApplication 启动类，实现基本的 Spring Boot 应用启动功能

---
+++ xxl-job-plus-admin/src/main/java/com/ccexid/admin/XxlJobAdminApplication.java
@@ -0,0 +1,15 @@
+package com.ccexid.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class XxlJobAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobAdminApplication.class, args);
+    }
+}
